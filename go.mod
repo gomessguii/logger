df51f07e25aea7832f7de1567e2e6cfde2dfb2c0
Guilherme Oliveira <guinomotec.dev@gmail.com>
2024-04-10 12:24:02 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gomessguii/logger
+
+go 1.22.2
---
+++ logger.go
@@ -0,0 +1,48 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+const (
+	INFO  = iota
+	ERR   = iota
+	WARN  = iota
+	DEBUG = iota
+)
+
+func Log(logLevel int, format string, v ...any) {
+	if logLevel == DEBUG && os.Getenv("DEBUG_ENABLED") != "1" {
+		return
+	}
+	var preffix string
+	switch logLevel {
+	case ERR:
+		preffix = "\033[41m[ERR]\033[0m "
+	case WARN:
+		preffix = "\033[43m[WARN]\033[0m "
+	case DEBUG:
+		preffix = "\033[40m[DEBUG]\033[0m "
+	default:
+		preffix = "\033[44m[INFO]\033[0m "
+	}
+	preffix += format
+	log.Printf(preffix, v...)
+}
+
+func LogError(format string, v ...any) {
+	Log(ERR, format, v...)
+}
+
+func LogWarn(format string, v ...any) {
+	Log(WARN, format, v...)
+}
+
+func LogDebug(format string, v ...any) {
+	Log(DEBUG, format, v...)
+}
+
+func LogInfo(format string, v ...any) {
+	Log(INFO, format, v...)
+}
